510053d85545b9a8a70552276e3f241e21fdc6a0
Brenden D <oreo12589@gmail.com>
2021-10-25 21:59:00 -0400

    Test

---
+++ Assets/Scripts/ToxicSlime.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToxicSlime : MonoBehaviour
+{
+    public GameObject explosionEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 particleSpawnPoint = other.transform.position;
+        Instantiate(explosionEffect, particleSpawnPoint, Quaternion.identity);
+
+        //Destroy() is the base function for destroying components in a scene
+        //You need to specify that you are refferring to a game object.
+        //Destroy(this), for instance, will destroy the component, not the object.
+        //Destroy(this.gameObject) will destroy the object where this component is.
+        Destroy(other.gameObject);
+    }
+}
+
