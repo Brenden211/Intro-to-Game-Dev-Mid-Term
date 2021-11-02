17798729c13c0f5064e25a1878d94b08c181199f
Brenden D <oreo12589@gmail.com>
2021-11-02 03:33:09 -0400

    Final

--- Assets/Scripts/DestroyMode.cs
+++ Assets/Scripts/DestroyMode.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToxicSlime : MonoBehaviour
+public class DestroyMode : MonoBehaviour
 {
     public GameObject explosionEffect;
 
